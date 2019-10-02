25c3f57fe71e547ef2baacc06a30b20beae02d1a
rahumada <ricardo@enmotionvalue.com>
2019-10-02 20:41:12 +0200

    clean

--- src/main/java/com/howtodoinjava/demo/config/WebFluxConfig.java
+++
@@ -1,9 +0,0 @@
-package com.howtodoinjava.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableWebFlux
-public class WebFluxConfig implements WebFluxConfigurer
-{  
-}
