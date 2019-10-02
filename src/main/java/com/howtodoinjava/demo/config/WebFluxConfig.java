2d62589ed9d077a699cfe57b98b79279d9c898a1
rahumada <ricardo@enmotionvalue.com>
2019-10-02 20:34:23 +0200

    first commit

---
+++ src/main/java/com/howtodoinjava/demo/config/WebFluxConfig.java
@@ -0,0 +1,9 @@
+package com.howtodoinjava.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer
+{  
+}
